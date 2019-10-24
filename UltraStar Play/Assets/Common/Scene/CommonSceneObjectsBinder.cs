e6eb21b93ad6b0bfcfda6efbc5a89fc210208ce8
Andreas Stange <anst.dev@gmail.com>
2019-10-24 17:43:32 +0200

    Zenject installer for common scene objects

---
+++ UltraStar Play/Assets/Common/Scene/CommonSceneObjectsBinder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class CommonSceneObjectsBinder : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.BindInstance(I18NManager.Instance);
+        Container.BindInstance(ThemeManager.Instance);
+        Container.BindInstance(ApplicationManager.Instance);
+        Container.BindInstance(PlayerProfileManager.Instance);
+        Container.BindInstance(SongMetaManager.Instance);
+        Container.BindInstance(SceneNavigator.Instance);
+    }
+}
